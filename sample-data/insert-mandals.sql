c7c2a1f6817b9d9a00793f4a455ecef252ed6af1
Maddi Jitendra <maddikittu7@gmail.com>
2025-09-11 09:57:19 +0000

    removed all the sample data

--- sample-data/insert-mandals.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO mandals (id, label_english, label_telugu, created_at, updated_at) VALUES
-  ('7b4e1f58-3c0a-4c9a-9a2b-1d7c8e9f2a31', 'Raikode', 'రాయికోడ్', NOW(), NOW()),
-  ('2f6a9d1b-8c73-4f1e-bd3a-56f00a9e12cd', 'Nyalkal', 'న్యాల్కల్', NOW(), NOW()),
-  ('9a1c3e5f-2b47-4d6e-a90f-3f2e1d4c5b6a', 'Jharasangham', 'ఝరాసంగం', NOW(), NOW()),
-  ('c4d5e6f7-8a9b-4c0d-b1e2-3f4a5b6c7d8e', 'Zaheerabad', 'జహీరాబాద్', NOW(), NOW()),
-  ('0d1e2f3a-4b5c-4d6e-8f90-a1b2c3d4e5f6', 'Kohir', 'కోహిర్', NOW(), NOW()),
-  ('5e4d3c2b-1a0f-4e5d-9c8b-7a6f5e4d3c2b', 'Mogudampally', 'మోగుడంపల్లి', NOW(), NOW());
